5fba108458b13cfd6c16a2ea3540ed770e0dbec2
Terrence Ryan <tpryan@google.com>
2020-05-29 14:21:34 -0700

    Hiding toolbar on logjn

--- frontend/src/app/view/toolbar/toolbar.component.ts
+++ frontend/src/app/view/toolbar/toolbar.component.ts
@@ -12,10 +12,12 @@ export class ToolbarComponent implements OnInit {
   isAdmin:boolean = false;
   public games:any;
   public id:string;
+  public currentRoute:string;
 
   constructor(public auth:AuthService, public router:Router, route: ActivatedRoute,) {
     this.isAdmin = auth.isAdmin()
     this.id = route.snapshot.paramMap.get('id');
+    this.currentRoute = this.router.url; 
     if (this.id != null ){
       this.auth.checkGameAdmin(this.id);
     }
@@ -23,6 +25,17 @@ export class ToolbarComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  isLogin(){
+    if (this.currentRoute.includes("invite")){
+      return true;
+    }
+    if (this.currentRoute.includes("login")){
+      return true;
+    }
+    
+    return false;
   }
 
   logout(){
